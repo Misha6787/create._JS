dd2131c6f317123c017771626ceb55f46d84a47f
Filimon Mikhail <misafilimon255@mail.ru>
2020-06-26 11:39:46 +0300

    feature(js/script.js)redid the logic

--- js/script.js
+++ js/script.js
@@ -1,62 +1,34 @@
 'use strict';
-const   input = document.querySelector('#input'),
-        textArea = document.querySelector('.textArea'),
-        button = document.querySelector('.button'),
-        heightInput = document.querySelector('.heightInput'),
-        widthInput = document.querySelector('.widthInput'),
-        bgInput = document.querySelector('.bgInput'),
-        fontSizeInput = document.querySelector('.fontSizeInput'),
-        addStyle = document.querySelector('.addStyle');
+const   blockCreateElem = document.querySelector('.blockCreateElem');
 
-const DomElement = function(){
-    DomElement.prototype.createElem = function(selector, height, width, bg, fontSize){
-        this.selector = selector;
-        this.height = height; 
-        this.width = width; 
-        this.bg = bg; 
-        this.fontSize = fontSize;
-        if(this.selector.substring(0, 1) === '.'){
-            const div = document.createElement('div');
-            const text = this.selector.slice(1);
-
-            div.setAttribute('class',text);
-            div.innerHTML = `Hello_Wolrd, it's me! ${text}`;
-            textArea.append(div);
-
-        } else if(this.selector.substring(0, 1) === '#'){
-            const id = document.createElement('div');
-            const text = this.selector.slice(1);
-
-            id.setAttribute('id',text);
-            id.innerHTML = `Hello_Wolrd, it's me! ${text}`;
-            textArea.append(id);
-
-        }
-        input.value = '';
-        textArea.style.cssText = 
+// Загатовка class = selector = '.class', height = '40px', width = '120px', bg = 'blue', fontSize = '18px'
+function DomElement(selector = '#id', height = '24px', width = '100px', bg = 'red', fontSize = '20px'){
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+}
+    DomElement.prototype.createElem = function(){
+        let block;
+        const text = this.selector.slice(1);
+        this.selector.substring(0, 1) === '.' ? block = document.createElement('div') : block = document.createElement('p');
+        this.selector.substring(0, 1) === '.' ? block.setAttribute('class',text) : block.setAttribute('id', text);
+        block.style.cssText = 
         `
         height: ${this.height};
         width: ${this.width};
         background-color: ${this.bg};
         font-size: ${this.fontSize};
         `;
+        block.innerHTML = `${text}`;
+        blockCreateElem.append(block);
     };
-};
-let domElement;
+const domElement = new DomElement();
 
-button.addEventListener('click', function(){
-    const selector = input.value;
-    const height = heightInput.value;
-    const width = widthInput.value;
-    const bg = bgInput.value;
-    const fontSize = fontSizeInput.value;
-
-    domElement = new DomElement();
-    return DomElement.prototype.createElem(selector, height, width, bg, fontSize);
-});
+domElement.createElem();
 
 const check = document.querySelector('.check');
 check.addEventListener('click', function(){
     console.log(domElement);
 });
-
