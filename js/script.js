643cb1594969419067a814e3e6a07d542a09301c
Filimon Mikhail <misafilimon255@mail.ru>
2020-06-26 11:55:19 +0300

    feature(js/script.js, index.html)add button and input for create elem

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 'use strict';
 const   blockCreateElem = document.querySelector('.blockCreateElem');
-
+const   inp = document.querySelector('#inp');
+const   button = document.querySelector('button');
 // Загатовка class = selector = '.class', height = '40px', width = '120px', bg = 'blue', fontSize = '18px'
-function DomElement(selector = '#id', height = '24px', width = '100px', bg = 'red', fontSize = '20px'){
-    this.selector = selector;
+function DomElement( height = '24px', width = '100px', bg = 'red', fontSize = '20px'){
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
 }
-    DomElement.prototype.createElem = function(){
+    DomElement.prototype.createElem = function(selector){
+        this.selector = selector;
         let block;
         const text = this.selector.slice(1);
         this.selector.substring(0, 1) === '.' ? block = document.createElement('div') : block = document.createElement('p');
@@ -25,8 +26,11 @@ function DomElement(selector = '#id', height = '24px', width = '100px', bg = 're
         blockCreateElem.append(block);
     };
 const domElement = new DomElement();
+button.addEventListener('click', function () { 
+    const selector = inp.value;
+    domElement.createElem(selector);
+});
 
-domElement.createElem();
 
 const check = document.querySelector('.check');
 check.addEventListener('click', function(){
