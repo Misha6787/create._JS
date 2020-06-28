5ebb175abb8251440a840f9bcc8abb8d14ab32ab
Filimon Mikhail <misafilimon255@mail.ru>
2020-06-28 11:41:09 +0300

    fixed(js/script.js)remove old version and changed innerHTML

--- js/script.js
+++ js/script.js
@@ -3,44 +3,30 @@ const   blockCreateElem = document.querySelector('.blockCreateElem');
 const   inp = document.querySelector('#inp');
 const   button = document.querySelector('button');
 
-function DomElement( selector = '.c', height = '24px', width = '100px', bg = 'red', fontSize = '20px' ){
+function DomElement( selector = '.class', height = '24px', width = '100px', bg = 'red', fontSize = '20px' ){
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
 }
-    DomElement.prototype.createElem = function( selector ){
-        if(selector === ''){
-            console.log('Берем дэфолтное значение');
-        } else {
+    DomElement.prototype.createElem = function(selector){
+        if(selector !== ''){
             this.selector = selector;
         }
+        let block;
         const text = this.selector.slice(1);
-        if(this.selector.substring(0, 1) === '.'){
-            blockCreateElem.insertAdjacentHTML('beforeend', `
-            <style>
-                .${text}{
-                    height: ${this.height};
-                    width: ${this.width};
-                    background-color: ${this.bg};
-                    font-size: ${this.fontSize};
-                }
-            </style>
-            <div class="${text}">${text}</div>
-            `);
-            } else if(this.selector.substring(0, 1) === '#'){ 
-            blockCreateElem.insertAdjacentHTML('beforeend', `
-                <style>
-                #${text}{
-                    height: ${this.height};
-                    width: ${this.width};
-                    background-color: ${this.bg};
-                    font-size: ${this.fontSize};
-                }
-                </style>
-            <p id="${text}">${text}</p>`);
-            }
+        this.selector.substring(0, 1) === '.' ? block = document.createElement('div') : block = document.createElement('p');
+        this.selector.substring(0, 1) === '.' ? block.setAttribute('class',text) : block.setAttribute('id', text);
+        block.style.cssText = 
+        `
+        height: ${this.height};
+        width: ${this.width};
+        background-color: ${this.bg};
+        font-size: ${this.fontSize};
+        `;
+        block.textContent = text;
+        blockCreateElem.append(block);
     };
 const domElement = new DomElement();
 button.addEventListener('click', function () { 
